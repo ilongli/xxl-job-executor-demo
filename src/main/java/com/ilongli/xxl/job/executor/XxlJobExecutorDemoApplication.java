3fabed480af4593a005bb15a12ee7dda5b29e28c
ilongli <351365415@qq.com>
2023-04-23 09:01:35 +0800

    Initial commit

---
+++ src/main/java/com/ilongli/xxl/job/executor/XxlJobExecutorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ilongli.xxl.job.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobExecutorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilongli/xxl/job/executor/XxlJobExecutorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilongli.xxl.job.executor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxlJobExecutorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
